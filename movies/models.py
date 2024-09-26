1eb956aa8ea53e9a98194a6b71c53ff90b1377f9
Vitaly_D <vitcode.mail@gmail.com>
2024-09-26 22:42:48 +0300

    Создана модель таблицы content.genre

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,24 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    # Типичная модель в Django использует число в качестве id. В таких ситуациях поле не описывается в модели.
+    # Вам же придётся явно объявить primary key.
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # Первым аргументом обычно идёт человекочитаемое название поля
+    name = models.CharField("name", max_length=255)
+    # blank=True делает поле необязательным для заполнения.
+    description = models.TextField("description", blank=True)
+    # auto_now_add автоматически выставит дату создания записи
+    created = models.DateTimeField(auto_now_add=True)
+    # auto_now изменятся при каждом обновлении записи
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # Ваши таблицы находятся в нестандартной схеме. Это нужно указать в классе модели
+        db_table = "content'.'genre"
+        # Следующие два поля отвечают за название модели в интерфейсе
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
