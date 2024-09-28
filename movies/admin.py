525d021b28c45766faa1adafe49c8c8da6bc97ad
Vitaly_D <vitcode.mail@gmail.com>
2024-09-28 17:56:00 +0300

    Настроена Админ панель (оформление, фильтры и поиск)

--- movies/admin.py
+++ movies/admin.py
@@ -5,7 +5,13 @@ from .models import Filmwork, Genre, GenreFilmwork, Person, PersonFilmwork
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "name",
+        "description",  ###
+        "modified",
+    )
+
+    search_fields = ("name", "description", "id")
 
 
 class GenreFilmworkInline(admin.TabularInline):
@@ -14,7 +20,13 @@ class GenreFilmworkInline(admin.TabularInline):
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "full_name",
+        "modified",
+    )
+
+    search_fields = ("full_name", "id")
+    # search_fields = ("full_name",)
 
 
 class PersonFilmworkInline(admin.TabularInline):
@@ -24,3 +36,17 @@ class PersonFilmworkInline(admin.TabularInline):
 @admin.register(Filmwork)
 class FilmworkAdmin(admin.ModelAdmin):
     inlines = (GenreFilmworkInline, PersonFilmworkInline)
+
+    # Отображение полей в списке
+    list_display = (
+        "title",
+        "type",
+        "creation_date",
+        "rating",
+    )
+
+    # Фильтрация в списке
+    list_filter = ("type",)
+
+    # Поиск по полям
+    search_fields = ("title", "description", "id")
