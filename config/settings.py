f6d6dd1b9a5ab5aea54014a42fcb9819fb51ba7a
Vitaly_D <vitcode.mail@gmail.com>
2024-10-07 16:19:11 +0300

    Версия 1 (Финал)

--- config/settings.py
+++ config/settings.py
@@ -12,8 +12,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", False) == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
-
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(",")
 
 # Приложения: config/components/applications.py
 include(
